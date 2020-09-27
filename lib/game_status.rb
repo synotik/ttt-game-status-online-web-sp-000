c21e9be94737c65c378697395e5ba3de2595a73c
Jeff Johnson <synotik@hotmail.com>
2020-09-27 20:04:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each {
+  WIN_COMBINATIONS.each do |win_combination|
 
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
@@ -32,5 +32,5 @@ def won?(board)
     else
       false
     end
-  }
+  end
 end
