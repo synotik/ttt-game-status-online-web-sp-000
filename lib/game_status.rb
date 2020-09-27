5781237607db132c283a269abf5b3e5b9107f44a
Jeff Johnson <synotik@hotmail.com>
2020-09-27 20:31:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,9 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    else
-      false
     end
   end
+
+ false
+
 end
